0c2d8fcdb8a09d6110b76faeb84dd0bf3feb468b
marinakomisarchuk <91791839+marinakomisarchuk@users.noreply.github.com>
2024-01-03 20:41:30 +0300

    Update Main.java

--- src/main/java/by/komisarchukmarina/Main.java
+++ src/main/java/by/komisarchukmarina/Main.java
@@ -2,6 +2,6 @@ package by.komisarchukmarina;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa_guru!");
+        System.out.println("Hello qa_guru!!!!!!");
     }
 }
