79684894296e14613c8aa62f3ce5202d0679c1a0
Marina Komisarchuk <komisarchukmarina.003@gmail.com>
2024-01-03 20:44:51 +0300

    added getMessage() method

--- src/main/java/by/komisarchukmarina/Main.java
+++ src/main/java/by/komisarchukmarina/Main.java
@@ -2,6 +2,10 @@ package by.komisarchukmarina;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa_guru!");
+        System.out.println(getMessage());
+    }
+
+    private static String getMessage() {
+        return "Hello qa_guru";
     }
 }
